29677c0e998f7a690fb73e5903f65d6f6839e392
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 17:16:18 +0530

    First commit

---
+++ OOPs_LineComparison.java
@@ -0,0 +1,9 @@
+package OPPs_LineComparison;
+
+public class OOPs_LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
