15ce617d3a7f606078d12e37caba5c7b298e6d20
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-04 17:36:34 +0530

    Checking Equal or not

--- OOPs_LineComparison.java
+++ OOPs_LineComparison.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class OOPs_LineComparison {
 
+	static double line1, line2;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
 
 		System.out.print("Enter End point of Line1:");
-		double line1 = lengthCal();
-
+		line1 = lengthCal();
+		line2 = lengthCal();
+		checkEqual(line2, line1);
 	}
 
 	private static double lengthCal() {
@@ -26,6 +29,13 @@ public class OOPs_LineComparison {
 		Double Length = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
 
 		return Length;
+	}
+
+	private static void checkEqual(double l1, double l2) {
+		if (l1 == l2) {
+			System.out.println("Lines are Equal");
+		} else
+			System.out.println("Lines are not Equal");
 	}
 
 }
