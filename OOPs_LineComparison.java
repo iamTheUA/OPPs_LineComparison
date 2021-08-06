f279afb712f1e84efad7b67c6d29f63171345e4d
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-06 11:12:47 +0530

    More refactored

--- OOPs_LineComparison.java
+++ OOPs_LineComparison.java
@@ -4,31 +4,15 @@ import java.util.Scanner;
 
 public class OOPs_LineComparison {
 
-	static double line1, line2;
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		OOPs_LineComparison ob = new OOPs_LineComparison();
+		System.out.println("Enter End points of Line1:");
+		Line line3 = ob.new Line();
+		System.out.println("Enter End points of Line2:");
+		Line line2 = ob.new Line();
 
-		System.out.println("Enter End point of Line1:");
-		line1 = lengthCal();
-		System.out.println("Enter End point of Line2:");
-		line2 = lengthCal();
-		checkEqual(line2, line1);
-	}
-
-	private static double lengthCal() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Point1's x=");
-		int x1 = sc.nextInt();
-		System.out.println("Enter Point1's y=");
-		int y1 = sc.nextInt();
-		System.out.println("Enter Point2's x=");
-		int x2 = sc.nextInt();
-		System.out.println("Enter Point2's y=");
-		int y2 = sc.nextInt();
-		Double Length = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
-
-		return Length;
+		checkEqual(line3.length, line2.length);
 	}
 
 	private static void checkEqual(double l1, double l2) {
@@ -45,5 +29,26 @@ public class OOPs_LineComparison {
 			System.out.println("Line1 is Greater");
 		} else
 			System.out.println("Line2 is Greater");
+	}
+
+	class Line {
+		public double length;
+		private int x1;
+		private int x2;
+		private int y1;
+		private int y2;
+
+		Line() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Point1's x=");
+			this.x1 = sc.nextInt();
+			System.out.println("Enter Point1's y=");
+			this.y1 = sc.nextInt();
+			System.out.println("Enter Point2's x=");
+			this.x2 = sc.nextInt();
+			System.out.println("Enter Point2's y=");
+			this.y2 = sc.nextInt();
+			this.length = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+		}
 	}
 }
